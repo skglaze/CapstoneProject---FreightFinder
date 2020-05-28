7ea930c5379d76297148d9f6840bd4c158da33e1
skglaze <skglaze@gmail.com>
2020-05-28 13:31:33 -0400

    added external mongodb. about to deploy

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.MONGODB_URI || 'mongodb://localhost/freightmover';
+const connectionString = process.env.MONGODB_URI || 'mongodb+srv://test:test@cluster0-qp860.gcp.mongodb.net/test?retryWrites=true&w=majority';
 
 mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
